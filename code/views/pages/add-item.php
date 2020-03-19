9b1f446d62bdee6488fa53d0b6f70d48c6fe18ae
Nazar Shershen <nazar.sherhsen@linkupst.com>
2020-03-19 19:46:41 +0200

    add css to adding form

--- code/views/pages/add-item.php
+++ code/views/pages/add-item.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
             <form action="" enctype="multipart/form-data" method="POST" class="create-artifact">
                 <div class="form-group">
                     <label for="title">Title:</label>
